9d7e309d5f7aaf923b65b79ea8f2cb3a6fabdda8
huisheng.jin <huisheng.jinge@gmail.com>
2021-06-07 22:49:34 +0800

    init project with spring.io.start

---
+++ src/main/java/com/example/hellocucumber/HellocucumberApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellocucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellocucumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellocucumberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellocucumber/HellocucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellocucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellocucumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
